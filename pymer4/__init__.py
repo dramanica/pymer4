aaef92aad038b35b969030c6a4bbca15d9a8e942
Andrea Manica <am315@cam.ac.uk>
2022-07-15 17:34:02 +0100

    Minimal package for segfault

--- pymer4/__init__.py
+++ pymer4/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __all__ = ["utils", "test_install", "__version__"]
 
-from .utils import get_resource_path, isPSD, nearestPSD, upper, R2con, con2R
+from .utils import get_resource_path
 
 
 from .version import __version__
