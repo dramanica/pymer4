96422f1168bcb85b779e306fbaa21e60ede8d330
Andrea Manica <am315@cam.ac.uk>
2022-07-15 17:14:37 +0100

    Start minimal crash package

--- pymer4/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Pymer4 model types.
-"""
-
-from .Lmer import Lmer
-from .Lm import Lm
-from .Lm2 import Lm2
-
-__all__ = ["Lmer", "Lm", "Lm2"]
--- pymer4/test_install.py
+++ pymer4/test_install.py
@@ -3,7 +3,6 @@ def test_install():
     Quick function to test installation by import a lmm object and fitting a quick model.
     """
     try:
-        from pymer4.models import Lmer
         from pymer4.utils import get_resource_path
         import os
         import pandas as pd
@@ -11,8 +10,8 @@ def test_install():
 
         warnings.filterwarnings("ignore")
         df = pd.read_csv(os.path.join(get_resource_path(), "sample_data.csv"))
-        model = Lmer("DV ~ IV3 + (1|Group)", data=df)
-        model.fit(summarize=False)
-        print("Pymer4 installation working successfully!")
+        #model = Lmer("DV ~ IV3 + (1|Group)", data=df)
+        #model.fit(summarize=False)
+        print("minimal Pymer4 installation working successfully!")
     except Exception as e:
         print("Error! {}".format(e))
